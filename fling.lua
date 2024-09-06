b3dd111cfc26b9809049f9e2cb83af12d5a1d301
Rayan511x55x <138740710+Rayan511x55x@users.noreply.github.com>
2024-09-06 20:32:29 +0300

    Update fling.lua

--- fling.lua
+++ fling.lua
@@ -1,6 +1,4 @@
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe./main/Fling%20GUI"))()
-
 --[[
  .____                  ________ ___.    _____                           __                
  |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
